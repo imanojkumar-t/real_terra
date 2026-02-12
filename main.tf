25f3f5a47459ba38cee21ea401142590e35d3648
manoj <iammanojkumar.t@gmail.com>
2026-02-12 20:08:40 +0530

    added the userdata to the instance

--- main.tf
+++ main.tf
@@ -5,9 +5,17 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = ""
   instance_type: ""
+  userdata = <<-EOF
+             #!/bin/bash
+             echo "Hello, World" > index.xhtml
+             nohup busybox httpd -f -p 8080 &
+             EOF
+  user_data__replace_on_change = true
 
 tags = {
   name = "terraform-example"
 }
 }
+
+
 
