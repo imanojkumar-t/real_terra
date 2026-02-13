b2571e9d90ba77368430509dc148e26a7e60e876
manoj <iammanojkumar.t@gmail.com>
2026-02-13 22:24:27 +0530

    added launch config insted of instance

--- main.tf
+++ main.tf
@@ -2,20 +2,15 @@ provider "aws" {
   region = "as-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami = ""
-  instance_type: ""
-  vpc_security_group_ids = [aws_security_group.instance.id]
-  userdata = <<-EOF
+resource "aws_launc_configuration" "example" {
+  image_id = "ami_0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+  user_data = <<-EOF
              #!/bin/bash
-             echo "Hello, World" > index.xhtml
+             echo "hello, world" > index.xhtml
              nohup busybox httpd -f -p ${var.server_port} &
              EOF
-  user_data__replace_on_change = true
-
-tags = {
-  name = "terraform-example"
-}
 }
 
 resource "aws_security_group" "instance" {
